98ea047ac6a317896f4681b916bfc8b19d681d9b
Mamadou DIABY <md832613@MI115-02.iem>
2025-01-15 15:45:33 +0100

    sc

--- app/src/main/java/com/diaby404/intership_manager_jetpack_compose/Login.kt
+++ app/src/main/java/com/diaby404/intership_manager_jetpack_compose/Login.kt
@@ -1,17 +1,87 @@
 package com.diaby404.intership_manager_jetpack_compose
 
 import android.view.Display
-import androidx.compose.material.ripple.rememberRipple
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 
 class Login {
 
 
 
+    @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     fun LoginScreen(){
-        var username by remember { muta}
+        var username by remember { mutableStateOf("") }
+        var password by remember { mutableStateOf("") }
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    colors = topAppBarColors(
+                        containerColor = MaterialTheme.colorScheme.primaryContainer,
+                        titleContentColor = MaterialTheme.colorScheme.primary,
+                    ),
+                    title = {
+                        Text("Top app bar")
+                    }
+                )
+            },
+            bottomBar = {
+                BottomAppBar(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    contentColor = MaterialTheme.colorScheme.primary,
+                ) {
+                    Text(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        textAlign = TextAlign.Center,
+                        text = "Bottom app bar",
+                    )
+                }
+            }
+        ) {
+            innerPadding ->
+            Box(
+                modifier = Modifier.padding(innerPadding)
+                    .fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ){
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    OutlinedTextField(
+                        label = { Text("Username") },
+                        value = username,
+                        onValueChange = { username = it },
+                        leadingIcon = { Icon(Icons.Default.Person, contentDescription = null ) }
+                    )
+                }
+            }
+        }
     }
 
      @Preview
