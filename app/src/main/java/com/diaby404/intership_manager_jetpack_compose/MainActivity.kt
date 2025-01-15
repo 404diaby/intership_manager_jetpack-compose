35cd96c866a217495ecaa117a5dacefef58fc6ae
Mamadou DIABY <md832613@MI115-02.iem>
2025-01-15 13:55:19 +0100

    fc

---
+++ app/src/main/java/com/diaby404/intership_manager_jetpack_compose/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.diaby404.intership_manager_jetpack_compose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.diaby404.intership_manager_jetpack_compose.ui.theme.Intership_manager_jetpack_composeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Intership_manager_jetpack_composeTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Intership_manager_jetpack_composeTheme {
+        Greeting("Android")
+    }
+}
